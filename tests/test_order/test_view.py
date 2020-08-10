20b5920b12e7b16b6ed163294f3fb46c5d90321f
Artem Sliusarenko <sliusarenko.artem@gmail.com>
2020-08-10 12:53:35 +0300

    python unittests

--- tests/test_order/test_view.py
+++ tests/test_order/test_view.py
@@ -1,11 +1,11 @@
 import unittest
-from dashboard import create_app
-
+# from dashboard import create_app
+from app import app
 class TestView(unittest.TestCase):
     def setUp(self):
-        app = create_app()
-        app.testing = True
-        self.app = app.test_client()
+        # app = create_app()
+        # app.testing = True
+        self.app = app.test_client(self)
     # executed after each test
     def tearDown(self):
         pass
@@ -13,7 +13,7 @@ class TestView(unittest.TestCase):
     def test_index(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(b'{"message":"erver is running!","status":"flask"}\n', response.data)
+        self.assertEqual(b'{"message":"Server is running!","status":"flask"}\n', response.data)
 
     def test_inde(self):
         response = self.app.get('/')
